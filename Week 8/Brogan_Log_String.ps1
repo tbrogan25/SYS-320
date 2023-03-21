6e27605ce0877494d770f6d61da8b403e2d2d3ad
Tim <timothy.brogan@mymail.champlain.edu>
2023-03-21 12:53:04 -0700

    Week 8: Intro to PowerShell

---
+++ Week 8/Brogan_Log_String.ps1
@@ -0,0 +1,10 @@
+﻿# Task: Create a prompt that allows the user to specify a keyword or phrase to search on
+# Find a string from your event logs to search on
+
+# List all the available Windows Event logs
+Get-EventLog -list
+
+# Create a prompt to allow user to select the Log to view
+$readLog = Read-host -Prompt "Please select a log to review from the list above"
+
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -match "password" } | Export-csv "C:\Users\timothy\Documents\passwordLogs.csv"
