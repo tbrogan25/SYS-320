914393e198bc8e834ee2ffd90bc790b1f8598b76
Tim <timothy.brogan@mymail.champlain.edu>
2023-03-21 13:14:57 -0700

    Week 8: Intro to PowerShell

--- Week 8/Brogan_Log_String.ps1
+++ Week 8/Brogan_Log_String.ps1
@@ -4,7 +4,11 @@
 # List all the available Windows Event logs
 Get-EventLog -list
 
-# Create a prompt to allow user to select the Log to view
+# Create a prompt to allow user to select the Log category to view
 $readLog = Read-host -Prompt "Please select a log to review from the list above"
 
-Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -match "password" } | Export-csv "C:\Users\timothy\Documents\passwordLogs.csv"
+# Allows user to select all logs from the aforementioned category that contain a user-specified string
+$stringLog = Read-host -Prompt "Choose a string to search for within the $readLog log category"
+
+# Gets the newest 40 logs from the user-specified log that contains the user-specified string and outputs to a file
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -match $stringLog } | Export-csv "C:\Users\timothy\Documents\${stringLog}Logs.csv"
