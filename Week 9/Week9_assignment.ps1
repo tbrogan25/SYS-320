ba6911fb5f37470b7d354fb693b57c061d0d295c
Tim <timothy.brogan@mymail.champlain.edu>
2023-03-29 18:00:42 -0700

    Week 9 Assignment

---
+++ Week 9/Week9_assignment.ps1
@@ -0,0 +1,25 @@
+﻿# Task: Grab the DHCP and DNS server information using a WMI class
+
+Get-WmiObject Win32_NetworkAdapterConfiguration | Select DHCPServer, DNSServerSearchOrder
+
+# Task: Export the list of running processes and services into respective files
+
+## Processes
+
+Get-Process | Export-Csv -Path "C:\processes.csv" -NoTypeInformation
+
+## Services
+
+Get-Service | Export-Csv -Path "C:\services.csv" -NoTypeInformation
+
+# Task: Start and stop Windows Calculator using the process name
+
+## Starting calc.exe
+
+Start-Process calc.exe
+
+## Stopping calc.exe
+
+#Stop-Process -Name Calculator
+
+
