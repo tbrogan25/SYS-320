e1500681e51481fe83e3fabdd94ab937c4e315d9
tbrogan25 <89802481+tbrogan25@users.noreply.github.com>
2023-04-04 18:10:38 -0400

    Add files via upload

---
+++ Brogan_Week10.ps1
@@ -0,0 +1,19 @@
+﻿do {
+    Write-Host "Please choose an option:"
+    Write-Host "1. View all services"
+    Write-Host "2. View running services"
+    Write-Host "3. View stopped services"
+    Write-Host "4. Quit"
+    $choice = Read-Host "Enter your choice ('all', 'running', 'stopped', 'quit')"
+
+    switch ($choice) {
+        'all' {Get-Service}
+        'running' {Get-Service | Where-Object {$_.Status -eq "Running"}}
+        'stopped' {Get-Service | Where-Object {$_.Status -eq "Stopped"}}
+        'quit' {break}
+        default {
+            Write-Host "Invalid choice. Please try again."
+        }
+    }
+
+} until ($choice -eq "quit")
