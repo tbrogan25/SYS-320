b8e5fbe4f233b0cd61bd786d37bb1bb1b862533b
Tim <timothy.brogan@mymail.champlain.edu>
2023-04-17 09:19:41 -0700

    Uploaded Week 13 script

---
+++ Week 13/Week13_Assignment.ps1
@@ -0,0 +1,14 @@
+﻿# Login to a remote SSH server
+New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
+
+while ($True) {
+
+    # Add a prompt to run commands
+    $the_cmd = read-host -Prompt "Please enter a command"
+
+    # Run command on remote SSH server
+    (Invoke-SSHCommand -index 0 $the_cmd).Output
+
+}
+
+Get-SCPFile -ComputerName '192.168.4.22' -Credential (Get-Credential sys320) -RemotePath '/home/sys320' -LocalFile '.\tedx.jpeg'
