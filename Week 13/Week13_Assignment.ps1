0a1dc5c135c7b1905ac515e9ece134dfaa4ab762
tbrogan25 <89802481+tbrogan25@users.noreply.github.com>
2023-04-17 12:31:12 -0400

    Update Week13_Assignment.ps1

--- Week 13/Week13_Assignment.ps1
+++ Week 13/Week13_Assignment.ps1
@@ -1,14 +1,15 @@
-﻿# Login to a remote SSH server
+﻿# Login to a remote SSH server with specified user credentials. You will be prompted for a password.
 New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-Credential sys320)
 
 while ($True) {
 
-    # Add a prompt to run commands
+    # Add a prompt to run user-specific commands
     $the_cmd = read-host -Prompt "Please enter a command"
 
-    # Run command on remote SSH server
+    # Run the aforementioned command on remote SSH server
     (Invoke-SSHCommand -index 0 $the_cmd).Output
 
 }
 
+# Retrieve a specific local file from a remote server using SCP
 Get-SCPFile -ComputerName '192.168.4.22' -Credential (Get-Credential sys320) -RemotePath '/home/sys320' -LocalFile '.\tedx.jpeg'
